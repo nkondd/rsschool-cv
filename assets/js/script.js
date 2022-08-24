d47a50223ec5c5a1eb55d697096ae6afed9967de
nkondd <nkond22@gmail.com>
2022-08-24 19:21:07 +0200

    feat: add navigation button

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,22 +1,47 @@
-'use strict';
+/*===== SHOW MENU =====*/
+const showMenu = (toggleId, navId) =>{
+    const toggle = document.getElementById(toggleId),
+    nav = document.getElementById(navId)
 
-window.addEventListener('DOMContentLoaded', () => {
-    const menuBtn = document.querySelector('.burger-button');
-    const menu = document.querySelector('.header-menu');
+// Validate that variables exist
+if(toggle && nav){
+    toggle.addEventListener('click', ()=>{
+        // Add the show-menu class to the div tag with the nav-menu class
+        nav.classList.toggle('show-menu')
+    })
+}
+}
+showMenu('nav-toggle','nav-menu')
 
-    menuBtn.addEventListener('click', togleClasses);
+/*===== REMOVE MOBILE MENU =====*/
 
-    function togleClasses() {
-        menuBtn.classList.toggle('burger-button_active');
-        menu.classList.toggle('header-menu_active');
-        menuBtn.classList.toggle('burger-button_rotate');
-    }
+const navLink = document.querySelectorAll('.header-link')
 
-    window.addEventListener('click', (e) => {
-        const isOverflow = e.target.classList;
-        if (!isOverflow.contains('header-menu-list') && !isOverflow.contains('burger-line') &&
-        !isOverflow.contains('burger-button') && menu.classList.contains('header-menu_active')) {
-            togleClasses();
+function linkAction(){
+    const navMenu = document.getElementById('nav-menu')
+    // Each header-link removes the show-menu class
+    navMenu.classList.remove('show-menu')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction))
+
+/*===== SCROLL SECTIONS ACTIVE LINK =====*/
+
+const sections = document.querySelectorAll('section[id]')
+
+function scrollActive(){
+    const scrollY = window.pageYOffset
+
+    sections.forEach(current =>{
+        const sectionHeight = current.offsetHeight
+        const sectionTop = current.offsetTop - 50;
+        sectionId = current.getAttribute('id')
+
+        if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+            document.querySelector('.nav_menu a[href*=' + sectionId + ']').classList.add('active-link')
+        }else{
+            document.querySelector('.nav_menu a[href*=' + sectionId + ']').classList.remove('active-link')
+    
         }
-    });
-});
+    })
+}
+window.addEventListener('scroll', scrollActive)
