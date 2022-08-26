5ef42fb2a63d53bbdcf9d91bddc91121b5162a78
nkondd <nkond22@gmail.com>
2022-08-26 15:45:22 +0200

    feat: add PDF generator

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,23 +1,23 @@
 /*===== SHOW MENU =====*/
-const showMenu = (toggleId, navId) =>{
+const showMenu = (toggleId, navId) => {
     const toggle = document.getElementById(toggleId),
-    nav = document.getElementById(navId)
+        nav = document.getElementById(navId)
 
-// Validate that variables exist
-if(toggle && nav){
-    toggle.addEventListener('click', ()=>{
-        // Add the show-menu class to the div tag with the nav-menu class
-        nav.classList.toggle('show-menu')
-    })
+    // Validate that variables exist
+    if (toggle && nav) {
+        toggle.addEventListener('click', () => {
+            // Add the show-menu class to the div tag with the nav-menu class
+            nav.classList.toggle('show-menu')
+        })
+    }
 }
-}
-showMenu('nav-toggle','nav-menu')
+showMenu('nav-toggle', 'nav-menu')
 
 /*===== REMOVE MOBILE MENU =====*/
 
 const navLink = document.querySelectorAll('.header-link')
 
-function linkAction(){
+function linkAction() {
     const navMenu = document.getElementById('nav-menu')
     // Each header-link removes the show-menu class
     navMenu.classList.remove('show-menu')
@@ -28,19 +28,19 @@ navLink.forEach(n => n.addEventListener('click', linkAction))
 
 const sections = document.querySelectorAll('section[id]')
 
-function scrollActive(){
+function scrollActive() {
     const scrollY = window.pageYOffset
 
-    sections.forEach(current =>{
+    sections.forEach(current => {
         const sectionHeight = current.offsetHeight
         const sectionTop = current.offsetTop - 50;
         sectionId = current.getAttribute('id')
 
-        if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+        if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
             document.querySelector('.nav_menu a[href*=' + sectionId + ']').classList.add('active-link')
-        }else{
+        } else {
             document.querySelector('.nav_menu a[href*=' + sectionId + ']').classList.remove('active-link')
-    
+
         }
     })
 }
@@ -48,9 +48,9 @@ window.addEventListener('scroll', scrollActive)
 
 /*===== SHOW SCROLL TOP =====*/
 
-function scrollTop(){
+function scrollTop() {
     const scrollTop = document.getElementById('scroll-top');
-    if(this.scrollY >= 200) scrollTop.classList.add('show-scroll'); else scrollTop.classList.remove('show-scroll')
+    if (this.scrollY >= 200) scrollTop.classList.add('show-scroll'); else scrollTop.classList.remove('show-scroll')
 }
 
 window.addEventListener('scroll', scrollTop)
@@ -78,4 +78,53 @@ themeButton.addEventListener('click', () => {
 
     localStorage.setItem('selected-theme', getCurrentTheme())
     localStorage.setItem('selected-icon', getCurrentIcon())
+})
+
+/*===== REDUCE THE SIZE AND PRINT ON AN A4 SHEET =====*/
+
+function scaleCv() {
+    document.body.classList.add('scale-cv')
+}
+
+/*===== REMOVE THE SIZE WHEN THE CV IS DOWNLOADED =====*/
+
+function removeScale() {
+    document.body.classList.remove('scale-cv')
+}
+
+/*===== GENERATE PDF =====*/
+
+// PDF generated area
+
+let areaCv = document.getElementById('area-cv')
+
+let resumeButton = document.getElementById('resume-button')
+
+// Html2pdf oprions
+
+
+// Function to call areaCv and Html2Pdf options
+
+function generateResume() {
+    const areaCv = document.getElementById('area-cv');
+    html2pdf()
+    .set({ html2canvas: { scale: 4 } })
+    .from(areaCv)
+    .save();
+}
+
+// When the button is clicked, it executes the three functions 
+
+resumeButton.addEventListener('click', () => {
+    // 1. The class .scale-cv is added to the body, where it reduces the size of the elements
+
+    scaleCv()
+
+    // 2. The PDF is generated
+
+    generateResume()
+
+    // 3. The .scale-cv class is removed from the body after 5 seconds to return to normal size.
+
+    setTimeout(removeScale, 5000)
 })
